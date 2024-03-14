2cb63195d6d1583db94eddb951463a110b0c7bcf
Marcus Hellams <mhellams@hotmail.com>
2024-03-14 14:09:07 -0400

    more updates

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const { PORT = '4000' } = process.env;
 
@@ -8,6 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('_api', app, document);
   await app.listen(+PORT, async () => {
     console.log(`Listening at ${await app.getUrl()} 🚀 💯`);
   });
